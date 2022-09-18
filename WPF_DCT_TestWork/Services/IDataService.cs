583f0511ee7f1896f2bfbce58f27163da8414dc6
pusteugene <73907720+pusteugene@users.noreply.github.com>
2022-09-19 02:29:47 +0300

    Add IDataService interface

---
+++ WPF_DCT_TestWork/Services/IDataService.cs
@@ -0,0 +1,17 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_DCT_TestWork.Services
+{
+    public interface IDataService
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T entity);
+        Task<T> Update(int id, T entity);
+        Task<bool> Delete(int id);
+    }
+}
