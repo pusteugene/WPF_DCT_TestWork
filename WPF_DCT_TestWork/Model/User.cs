bdf0170d61d8803ffb936474bf123eebfed9c2ac
pusteugene <73907720+pusteugene@users.noreply.github.com>
2022-09-19 01:52:34 +0300

    Add project files.

---
+++ WPF_DCT_TestWork/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_DCT_TestWork.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
