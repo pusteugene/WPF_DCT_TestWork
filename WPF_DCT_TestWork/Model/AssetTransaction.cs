f47845c7dbc64fc4fc960bb7e31376c778a07aab
pusteugene <73907720+pusteugene@users.noreply.github.com>
2022-09-19 02:14:22 +0300

    Add class AssetTransaction

---
+++ WPF_DCT_TestWork/Model/AssetTransaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_DCT_TestWork.Model
+{
+    public class AssetTransaction
+    {
+        public int Id { get; set; }
+        public Account Account { get; set; }
+        public bool IsPurchase { get; set; }
+        public int Amount { get; set; }
+    }
+}
