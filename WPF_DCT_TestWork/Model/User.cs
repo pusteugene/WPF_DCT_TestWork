a9e43694177f19e7410cc1e3df77ce24e52335ec
pusteugene <73907720+pusteugene@users.noreply.github.com>
2022-09-19 03:41:08 +0300

    update user properties

--- WPF_DCT_TestWork/Model/User.cs
+++ WPF_DCT_TestWork/Model/User.cs
@@ -9,5 +9,7 @@ namespace WPF_DCT_TestWork.Model
         public int Id { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime DatedJoined { get; set; }
     }
 }
