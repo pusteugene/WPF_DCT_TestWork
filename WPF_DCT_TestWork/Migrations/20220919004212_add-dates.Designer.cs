a3d542c4b8c66475f70fc7960b10722228d6c089
pusteugene <73907720+pusteugene@users.noreply.github.com>
2022-09-19 03:42:35 +0300

    add new migrations for dates

--- WPF_DCT_TestWork/Migrations/20220919004212_add-dates.Designer.cs
+++ WPF_DCT_TestWork/Migrations/20220919004212_add-dates.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WPF_DCT_TestWork.DB;
 
 namespace WPF_DCT_TestWork.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20220919004212_add-dates")]
+    partial class adddates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
