80134c7c9ed29e47b4ea8f7a3797af116e53c9fc
pusteugene <73907720+pusteugene@users.noreply.github.com>
2022-09-19 03:21:44 +0300

    Add DBContext for connection to database

---
+++ WPF_DCT_TestWork/DB/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF_DCT_TestWork.Model;
+
+namespace WPF_DCT_TestWork.DB
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\localhost;Database=DCT_TestWork_base;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
